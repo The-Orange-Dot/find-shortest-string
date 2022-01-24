294d0dbf3e7f2e39a95969ada5f97c6edd10b74c
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-24 22:56:28 +0900

    Completed

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,21 +1,25 @@
 function findShortestString(arr) {
-  // type your code here
+  let sorted = arr.sort((a, b) => (a.length < b.length ? -1 : 1));
+  return sorted[0];
 }
 
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 'a'");
-  console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
+  console.log("=>", findShortestString(["aaa", "a", "bb", "ccc"]));
 
   console.log("");
 
   console.log("Expecting: 'hi'");
-  console.log("=>", findShortestString(['cat', 'hi', 'dog', 'an']));
+  console.log("=>", findShortestString(["cat", "hi", "dog", "an"]));
 
   console.log("");
 
   console.log("Expecting: 'lily'");
-  console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
+  console.log(
+    "=>",
+    findShortestString(["flower", "juniper", "lily", "dadelion"])
+  );
 
   // BENCHMARK HERE
 }
@@ -24,3 +28,8 @@ module.exports = findShortestString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+//Using the sort function, we check betwen two items in the array, and check their length.
+//The shorter length would get sent to the front of the new array while the longer would get sent to the back.
+
+//From there, once the array is sorted and put into a new variable, we take the first index [0] and return it
